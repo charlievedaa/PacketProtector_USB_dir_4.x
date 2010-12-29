627687dd36163dc3325007b9ed322e35fed0e29a
ciscostu <charlievedaa@users.noreply.github.com>
2010-12-29 04:02:13 +0000

    added openvpn iptables rules

---
+++ packetprotector/usr/sbin/openvpn_allow.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CONFIG_FILE="/etc/packetprotector.conf"
+OPENVPN=`grep "openvpn=" $CONFIG_FILE | cut -d "=" -f 2`
+LAN=`/sbin/uci get network.lan.ifname`
+if [ "X$OPENVPN" = "X1" ] ; then
+	iptables -I forward 1 -i $LAN -o tun0 -j ACCEPT
+	iptables -I forward 2 -i tun0 -o $LAN -j ACCEPT
+fi
