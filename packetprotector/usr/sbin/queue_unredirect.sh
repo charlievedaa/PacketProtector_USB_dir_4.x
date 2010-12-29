7921f387a0e9acc9d39a68a078b88a9319af82c9
ciscostu <charlievedaa@users.noreply.github.com>
2010-12-29 03:44:41 +0000

    update queue rules

--- packetprotector/usr/sbin/queue_unredirect.sh
+++ packetprotector/usr/sbin/queue_unredirect.sh
@@ -2,8 +2,9 @@
 
 CONFIG_FILE="/etc/packetprotector.conf"
 SNORT_INLINE=`grep "snort-inline=" $CONFIG_FILE | cut -d "=" -f 2`
+WAN=`/sbin/uci get network.wan.ifname`
 if [ "X$SNORT_INLINE" = "X1" ] ; then
 	iptables -R FORWARD 2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+	iptables -R zone_wan_ACCEPT 1 -o $WAN -j ACCEPT
 	iptables -R INPUT 1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-	iptables -R OUTPUT 1 -m state --state RELATED,ESTABLISHED -j ACCEPT	
 fi
